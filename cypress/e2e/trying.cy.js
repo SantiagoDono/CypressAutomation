154fe35f11ea92e4a6a4d521dafb514da707d303
donos <donos@SANTI-PC>
2023-07-14 20:38:44 -0300

    first commit

---
+++ cypress/e2e/trying.cy.js
@@ -0,0 +1,45 @@
+describe('Verificar que el placeholder sea correcto', () => {
+  it('place holder es correcto', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('input[placeholder="Search for Vegetables and Fruits"]')
+  })
+})
+describe('Cuantos productos que empiecen con Ca hay', () => {
+  it('hay un total de 4?', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.get('.product:visible').should('have.length', 4)// el :visible es para que solo matchee con lo que podemos ver
+    // en este caso si no ponemos visible la prueba falla 
+  })
+})
+
+describe('Nombre de los productos', () => {
+  it('producto numero uno su nombre es Cauliflower - 1 Kg', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.get('.products > :nth-child(1) > .product-name').contains('Cauliflower - 1 Kg')
+    //otra forma de hacerlo
+    cy.get('.products').find('.product').eq(1).contains('Carrot - 1 Kg')
+  })
+})
+describe('agregar al carrito ', () => {
+  it('Zanahorias v1', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.get('.product:visible').should('have.length', 4)
+    cy.get('.products').find('.product:visible').should('have.length', 4)
+    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+  })
+  it.only('Zanahorias Iterando', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+      const textVeg = $el.find('h4.product-name').text()
+      if (textVeg.includes('Carrot')) {
+        cy.wrap($el).find('button').contains('ADD TO CART').click()
+      }
+    })
+
+
+  })
+})
